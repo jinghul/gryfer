fca4fcc9dd306f80672b125ae506d47ff256df14
Ethan Zou <eazy975@gmail.com>
2019-03-09 20:17:49 +0800

    backend working

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const pg = require('pg')
+const db = require('./queries')
+
+// const conString = "postgres://" + config.username + ":" + config.password + "@localhost:5432/" + config.api
+
+// var client = new pg.Client(conString)
+// client.connect();
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
