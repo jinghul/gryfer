2f8eb082eacd500cfc17609c3e4b8da1508dd08e
jinghu <jinghul@bu.edu>
2019-03-10 22:05:11 +0800

    animation update

---
+++ assets/js/auth.js
@@ -0,0 +1,30 @@
+// Change from the sign-in page to register
+function si_to_reg() {
+    $('#sign-in').hide(300);
+    $('#auth-splash').fadeOut(200, function() {
+        $('#auth-splash').css('background-image', 'url(./img/reg-splash.jpg');
+    });
+    $('#auth-splash').fadeIn(200);
+    $('#register').show(300);
+}
+
+// Change from the register page to sign-in
+function reg_to_si() {
+    $('#register').hide(300);
+    $('#auth-splash').fadeOut(200, function() {
+        $('#auth-splash').css('background-image', 'url(./img/signin-splash.jpeg');
+    });
+    $('#auth-splash').fadeIn(200);
+    $('#sign-in').show(300);
+}
+
+// Register a user into the database
+function register() {}
+
+// Sign an user into the system
+function signin() {}
+
+$('document').ready(function() {
+    $('#si-to-reg').bind('click', si_to_reg);
+    $('#reg-to-si').bind('click', reg_to_si);
+});
