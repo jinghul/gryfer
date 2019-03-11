76041d84d0e1b588886634f69c04366dc3fb98c5
jinghu <jinghul@bu.edu>
2019-03-11 23:31:30 +0800

    handlebars init

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,20 +1,20 @@
 // Change from the sign-in page to register
 function si_to_reg() {
     $('#sign-in').hide(300);
-    $('#auth-splash').fadeOut(200, function() {
+    $('#auth-splash').animate({"left" : "-100%", "opacity" : "toggle"}, 300, "linear", function() {
         $('#auth-splash').css('background-image', 'url(./img/reg-splash.jpg');
     });
-    $('#auth-splash').fadeIn(200);
+    $('#auth-splash').animate({"left" : "0", "opacity" : "toggle"}, 200);
     $('#register').show(300);
 }
 
 // Change from the register page to sign-in
 function reg_to_si() {
     $('#register').hide(300);
-    $('#auth-splash').fadeOut(200, function() {
+    $('#auth-splash').animate({"left" : "-100%", "opacity" : "toggle"}, 300, "linear", function() {
         $('#auth-splash').css('background-image', 'url(./img/signin-splash.jpeg');
     });
-    $('#auth-splash').fadeIn(200);
+    $('#auth-splash').animate({"left" : "0", "opacity" : "toggle"}, 200);
     $('#sign-in').show(300);
 }
 
