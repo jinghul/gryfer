a914792efc638f7af3ef4d36e5f412783b2d173b
jinghu <jinghul@bu.edu>
2019-03-12 18:19:20 +0800

    register update

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,21 +1,29 @@
 // Change from the sign-in page to register
 function si_to_reg() {
     $('#sign-in').hide(300);
-    $('#auth-splash').animate({"left" : "-100%", "opacity" : "toggle"}, 300, "linear", function() {
-        $('#auth-splash').css('background-image', 'url(./img/reg-splash.jpg');
-    });
-    $('#auth-splash').animate({"left" : "0", "opacity" : "toggle"}, 200);
-    $('#register').show(300);
+    // $('#auth-splash').hide(300, function() {
+    // });
+    $('#auth-splash').animate(
+        { left: '-100%'},
+        300,
+        'linear',
+        function() {
+            $('#auth-splash').css('display', 'none');
+            $('#register').show(300);
+        }
+    );
 }
 
 // Change from the register page to sign-in
 function reg_to_si() {
-    $('#register').hide(300);
-    $('#auth-splash').animate({"left" : "-100%", "opacity" : "toggle"}, 300, "linear", function() {
-        $('#auth-splash').css('background-image', 'url(./img/signin-splash.jpeg');
+    $('#register').hide(300, function() {
+        $('#auth-splash').css('display', 'flex');
+        $('#auth-splash').animate(
+            { left: '0' },
+            200,
+        );
+        $('#sign-in').show(300);
     });
-    $('#auth-splash').animate({"left" : "0", "opacity" : "toggle"}, 200);
-    $('#sign-in').show(300);
 }
 
 // Register a user into the database
@@ -24,7 +32,26 @@ function register() {}
 // Sign an user into the system
 function signin() {}
 
+// Reset user entries
+function reset(form) {}
+
 $('document').ready(function() {
     $('#si-to-reg').bind('click', si_to_reg);
     $('#reg-to-si').bind('click', reg_to_si);
+
+    // TODO: More validation
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener(
+            'submit',
+            function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            },
+            false
+        );
+    });
 });
