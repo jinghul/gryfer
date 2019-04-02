2fcde2cd129a21b84a7b36333cb70ff9ef7626f4
burstein <alanbur@bu.edu>
2019-04-02 17:42:11 +0800

    minor change

--- db/migrations/20190328165824_triggers.js
+++ db/migrations/20190328165824_triggers.js
@@ -1,6 +1,27 @@
-
 exports.up = function(knex, Promise) {
   
+  let createQuery = `
+    CREATE OR REPLACE FUNCTION accept_bid ()
+    RETURNS TRIGGER AS
+    $$
+    BEGIN
+        INSERT INTO
+            History(username, aid)
+            VALUES(new.uid,new.aid);
+               RETURN new;
+    END;
+    $$
+    LANGUAGE plpgsql ;
+
+    CREATE TRIGGER accept_bid
+    AFTER INSERT OR UPDATE
+    ON Accepted
+    FOR EACH ROW
+    EXECUTE PROCEDURE acceept_bid();
+    `;
+  return knex.raw(createQuery);
+
+
 };
 
 exports.down = function(knex, Promise) {
