8718823d2c5e969be279c197f82b6d1816b87a7e
Ethan Zou <eazy975@gmail.com>
2019-04-11 20:01:05 +0800

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -110,14 +110,6 @@ CREATE TABLE CarProfiles (
     FOREIGN KEY (cid) REFERENCES Cars
 );
 
-CREATE TABLE "session" (
-    "sid" varchar NOT NULL COLLATE "default",
-    "sess" json NOT NULL,
-    "expire" timestamp(6) NOT NULL
-  )
-  WITH (OIDS=FALSE);
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
 CREATE TABLE Accepted (
     aid             INTEGER NOT NULL,
     puid            INTEGER NOT NULL,
